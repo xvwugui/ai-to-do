701ac6856bfa4a2bfe40512dc6936ac4c005e9ed
Tsomoore <tsomoore3@gmail.com>
2025-10-13 09:46:29 +0800

    work with json now

---
+++ core.py
@@ -0,0 +1,19 @@
+import json
+from pathlib import Path
+from typing import List, Dict, Any
+
+DB_PATH = Path.home() / ".ai_todo_db.json"
+
+def read_tasks() -> List[Dict[str, Any]]:
+    if not DB_PATH.is_file():
+        return []
+    with DB_PATH.open("r") as f:
+        try:
+            tasks = json.load(f)
+            return tasks
+        except json.JSONDecoderError:
+            return []
+
+def write_tasks(tasks : List[Dict[str, Any]]):
+    with DB_PATH.open("w") as f:
+        json.dump(tasks, f, indent = 4)
